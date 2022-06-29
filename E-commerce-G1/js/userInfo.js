4e70399452ac3d7e6c8d39cf17ae0eb190463601
Alberto Nuñez <76263107+Tuli1985@users.noreply.github.com>
2022-06-29 19:59:38 -0300

    Add files via upload
    Esta es una aplicacion de e commerce creada por el grupo 1 de el curso de testing avanzado con el objetivo de crear un set de pruebas sobre el mismo.

---
+++ E-commerce-G1/js/userInfo.js
@@ -0,0 +1,31 @@
+//DOM
+const user_info = document.getElementById('user_info');
+
+const settings = document.getElementById('settings');
+const logout = document.getElementById('logout');
+const login = document.getElementById('login');
+const signUp = document.getElementById('signUp');
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+//user logs in
+      user_info.innerHTML = user.email;
+      settings.style.display = 'block';
+      logout.style.display = 'block';
+      login.style.display = 'none';
+      signUp.style.display = 'none';
+     
+
+  }else {
+    user_info.innerHTML = 'Hola,Inicia sesión';
+    settings.style.display = 'none';
+    logout.style.display = 'none';
+    login.style.display = 'block';
+    signUp.style.display = 'block';
+    //user logs out
+  }
+});
+
+logout.addEventListener('click', e => {
+  firebase.auth().signOut();
+});
