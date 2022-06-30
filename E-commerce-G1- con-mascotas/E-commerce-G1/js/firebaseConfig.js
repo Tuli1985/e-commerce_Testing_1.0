9ab8518185817c805604da2c3911e5244669a539
Alberto Nuñez <76263107+Tuli1985@users.noreply.github.com>
2022-06-30 14:38:37 -0300

    Add files via upload
    Esta es una aplicacion e-commerce que fue creada para el curso de testing avanzado, esta version incluye la seccion mascotas

---
+++ E-commerce-G1- con-mascotas/E-commerce-G1/js/firebaseConfig.js
@@ -0,0 +1,13 @@
+var config = {
+
+  apiKey: "AIzaSyC4I619Doq1rLCYu6KeKXxmfAlD2Foaa-8",
+  authDomain: "baritesting.firebaseapp.com",
+  databaseURL: "https://baritesting-default-rtdb.firebaseio.com",
+  projectId: "baritesting",
+  storageBucket: "baritesting.appspot.com",
+  messagingSenderId: "595881870200",
+  appId: "1:595881870200:web:bce6203c7b30a75d1c3e8d"
+ 
+  };
+  firebase.initializeApp(config);
+  console.log(firebase);
